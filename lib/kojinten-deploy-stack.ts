08e79ff61eb1ee692dfc2426185ab71226141911
Yukitaka Maeda <yumaeda@gmail.com>
2019-09-28 04:45:34 +0900

    Use existing Docker image

--- lib/kojinten-deploy-stack.ts
+++ lib/kojinten-deploy-stack.ts
@@ -1,9 +1,30 @@
-import cdk = require('@aws-cdk/core');
+import { Construct, Stack, StackProps } from '@aws-cdk/core'
+import { Vpc } from '@aws-cdk/aws-ec2'
+import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs'
+import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
 
-export class KojintenDeployStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class KojintenDeployStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props)
 
-    // The code that defines your stack goes here
-  }
+        // ECS Cluster
+        const cluster = new Cluster(this, 'prod-kojinten', {
+            vpc: new Vpc(this, 'prod-kojinten-vpc', {
+                maxAzs: 2
+            })
+        })
+
+        // ECR Image
+        const image = ContainerImage.fromRegistry('tiangolo/uwsgi-nginx-flask')
+
+        // Create a load-balanced Fargate service and make it public
+        new ApplicationLoadBalancedFargateService(this, 'KojintenApiService', {
+            cluster,
+            cpu: 256,
+            desiredCount: 1,
+            image,
+            memoryLimitMiB: 512,
+            publicLoadBalancer: true
+        })
+    }
 }
