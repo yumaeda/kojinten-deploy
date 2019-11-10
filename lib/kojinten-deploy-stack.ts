53d468aa0e51323cbbc09ac6446d55f8d716fde5
Yukitaka Maeda <yumaeda@gmail.com>
2019-11-10 16:25:45 +0900

    Set vpcPlacement to public subnet

--- lib/kojinten-deploy-stack.ts
+++ lib/kojinten-deploy-stack.ts
@@ -1,5 +1,19 @@
-import { Construct, RemovalPolicy, SecretValue, Stack, StackProps } from '@aws-cdk/core'
-import { Vpc, InstanceType, InstanceClass, InstanceSize } from '@aws-cdk/aws-ec2'
+import {
+    InstanceClass,
+    InstanceSize,
+    InstanceType,
+    SubnetType,
+    Vpc
+} from '@aws-cdk/aws-ec2'
+
+import {
+    Construct,
+    RemovalPolicy,
+    SecretValue,
+    Stack,
+    StackProps
+} from '@aws-cdk/core'
+
 import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs'
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
 import { DatabaseInstance, DatabaseInstanceEngine } from '@aws-cdk/aws-rds'
@@ -8,20 +22,17 @@ export class KojintenDeployStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
-        // VPC
-        const vpc = new Vpc(this, 'prod-kojinten-vpc', {
-            maxAzs: 2
-        })
-
-        // ECS Cluster
+        // Initialize VPC, ECS cluster, and Docker image.
+        const vpc = new Vpc(this, 'prod-kojinten-vpc', { maxAzs: 2 })
         const cluster = new Cluster(this, 'prod-kojinten', { vpc })
+        const image = ContainerImage.fromRegistry('yumaeda/kojinten')
 
         // Create a load-balanced Fargate service and make it public
         new ApplicationLoadBalancedFargateService(this, 'KojintenApiService', {
             cluster,
             cpu: 256,
             desiredCount: 1,
-            taskImageOptions: { image: ContainerImage.fromRegistry("yumaeda/kojinten") },
+            taskImageOptions: { image },
             memoryLimitMiB: 512,
             publicLoadBalancer: true
         })
@@ -29,12 +40,13 @@ export class KojintenDeployStack extends Stack {
         // Create a DB insttance.
         const dbInstance = new DatabaseInstance(this, 'RDS', {
             deletionProtection: false,
-            removalPolicy: RemovalPolicy.DESTROY,
             engine: DatabaseInstanceEngine.MARIADB,
             instanceClass: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.SMALL),
             instanceIdentifier: 'kojinten',
             masterUsername: 'admin',
-            vpc
+            removalPolicy: RemovalPolicy.DESTROY,
+            vpc,
+            vpcPlacement: { subnetType: SubnetType.PUBLIC }
         });
 
         // Allow connections on default port from any IPV4
