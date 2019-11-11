225f63b73611e7b6f9cc4c523a6285620612b240
Yukitaka Maeda <yumaeda@gmail.com>
2019-11-11 19:14:18 +0900

    Add code to configure Route53

--- lib/kojinten-deploy-stack.ts
+++ lib/kojinten-deploy-stack.ts
@@ -17,6 +17,8 @@ import {
 import { Cluster, ContainerImage } from '@aws-cdk/aws-ecs'
 import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns'
 import { DatabaseInstance, DatabaseInstanceEngine } from '@aws-cdk/aws-rds'
+import { ARecord, PublicHostedZone, RecordTarget } from '@aws-cdk/aws-route53'
+import { LoadBalancerTarget } from '@aws-cdk/aws-route53-targets'
 
 export class KojintenDeployStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -26,15 +28,23 @@ export class KojintenDeployStack extends Stack {
         const vpc = new Vpc(this, 'prod-kojinten-vpc', { maxAzs: 2 })
         const cluster = new Cluster(this, 'prod-kojinten', { vpc })
         const image = ContainerImage.fromRegistry('yumaeda/kojinten')
+        const zone = new PublicHostedZone(this, 'HostedZone', {
+            zoneName: 'wine-producer.com'
+        })
 
         // Create a load-balanced Fargate service and make it public
-        new ApplicationLoadBalancedFargateService(this, 'KojintenApiService', {
+        const alb = new ApplicationLoadBalancedFargateService(this, 'KojintenApiService', {
             cluster,
             cpu: 256,
             desiredCount: 1,
             taskImageOptions: { image },
             memoryLimitMiB: 512,
             publicLoadBalancer: true
+        })
+
+        new ARecord(this, 'AliasRecord', {
+            zone,
+            target: RecordTarget.fromAlias(new LoadBalancerTarget(alb.loadBalancer))
         })
 
         // Create a DB insttance.
