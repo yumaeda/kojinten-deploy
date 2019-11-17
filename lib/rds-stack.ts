ecc6f20ef5f417fcba1e4750f07c6bbb2fc693b7
Yukitaka Maeda <yumaeda@gmail.com>
2019-11-17 17:53:50 +0900

    Create RDS instance from snapshot

--- lib/rds-stack.ts
+++ lib/rds-stack.ts
@@ -1,17 +1,19 @@
 import { Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core'
 import { InstanceClass, InstanceSize, InstanceType, SubnetType, IVpc } from '@aws-cdk/aws-ec2'
-import { DatabaseInstance, DatabaseInstanceEngine } from '@aws-cdk/aws-rds'
+import { DatabaseInstanceFromSnapshot, DatabaseInstanceEngine } from '@aws-cdk/aws-rds'
 
 export class RDSStack extends Stack {
     constructor(scope: Construct, id: string, vpc: IVpc, props?: StackProps) {
         super(scope, id, props)
 
         // Create a DB insttance.
-        const dbInstance = new DatabaseInstance(this, 'RDS', {
+        const dbInstance = new DatabaseInstanceFromSnapshot(this, 'RDS', {
             deletionProtection: false,
             engine: DatabaseInstanceEngine.MARIADB,
+            generateMasterUserPassword: true,
             instanceClass: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.SMALL),
             instanceIdentifier: 'kojinten',
+            snapshotIdentifier: 'kojinten-20191117',
             masterUsername: 'admin',
             removalPolicy: RemovalPolicy.DESTROY,
             vpc,
