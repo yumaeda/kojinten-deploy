5bddd93d5a7a92ae5feec4c08bfccf5e85089820
Yukitaka Maeda <yumaeda@gmail.com>
2020-02-12 21:05:37 +0900

    Add certificate from ACM to CloudFront

--- lib/cloud-front-stack.ts
+++ lib/cloud-front-stack.ts
@@ -7,11 +7,20 @@ export class CloudFrontStack extends Stack {
     constructor(scope: Construct, id: string, sourceBucket: Bucket, props?: StackProps) {
         super(scope, id, props)
 
+        const certificateArn = 'arn:aws:acm:us-east-1:823135059493:certificate/00439ffb-ad01-497e-a9af-df4098249ae5'
         new CloudFrontWebDistribution(this, 'CloudFront', {
             originConfigs: [{
               s3OriginSource: { s3BucketSource: sourceBucket },
               behaviors: [{ isDefaultBehavior: true }]
-            }]
+            }],
+            viewerCertificate: ViewerCertificate.fromAcmCertificate(
+                Certificate.fromCertificateArn(this, 'Certificate', certificateArn),
+                {
+                    aliases: ['tokyo-hideaway.com', 'www.tokyo-hideaway.com'],
+                    securityPolicy: SecurityPolicyProtocol.TLS_V1,
+                    sslMethod: SSLMethod.SNI
+                }
+            )
         })
     }
 }
