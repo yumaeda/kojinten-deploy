fc2ec3bce79ebe5aa40f89a11c770fcb10bcc071
Yukitaka Maeda <yumaeda@gmail.com>
2020-03-01 17:02:23 +0900

    Register CNAME for the CloudFront domain

--- lib/cloud-front-stack.ts
+++ lib/cloud-front-stack.ts
@@ -1,14 +1,19 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core'
+import { Construct, CfnOutput, Stack, StackProps } from '@aws-cdk/core'
 import { CloudFrontWebDistribution, SecurityPolicyProtocol, SSLMethod, ViewerCertificate } from '@aws-cdk/aws-cloudfront'
 import { Certificate } from '@aws-cdk/aws-certificatemanager'
 import { Bucket } from '@aws-cdk/aws-s3'
+import { CnameRecord, PublicHostedZone, RecordTarget } from '@aws-cdk/aws-route53'
 
 export class CloudFrontStack extends Stack {
     constructor(scope: Construct, id: string, sourceBucket: Bucket, props?: StackProps) {
         super(scope, id, props)
 
-        const certificateArn = 'arn:aws:acm:us-east-1:823135059493:certificate/00439ffb-ad01-497e-a9af-df4098249ae5'
-        new CloudFrontWebDistribution(this, 'CloudFront', {
+        const hostedZoneId = 'Z2H02ZWJT2N1X8'
+        const zoneName = 'tokyo-hideaway.com'
+        const cdnAlias = `cdn.${zoneName}`
+        const zone = PublicHostedZone.fromHostedZoneAttributes(this, id, { zoneName, hostedZoneId })
+        const certificateArn = 'arn:aws:acm:us-east-1:823135059493:certificate/28f984da-1950-4343-b347-773e991e9de4'
+        let cf = new CloudFrontWebDistribution(this, 'CloudFront', {
             originConfigs: [{
               s3OriginSource: { s3BucketSource: sourceBucket },
               behaviors: [{ isDefaultBehavior: true }]
@@ -16,11 +21,17 @@ export class CloudFrontStack extends Stack {
             viewerCertificate: ViewerCertificate.fromAcmCertificate(
                 Certificate.fromCertificateArn(this, 'Certificate', certificateArn),
                 {
-                    aliases: ['tokyo-hideaway.com', 'www.tokyo-hideaway.com'],
+                    aliases: [cdnAlias],
                     securityPolicy: SecurityPolicyProtocol.TLS_V1,
                     sslMethod: SSLMethod.SNI
                 }
             )
+        })
+        new CfnOutput(this, "CloudFront Domain", {value: cf.domainName});
+        new CnameRecord(this, 'CnameRecord', {
+            recordName: cdnAlias,
+            zone,
+            domainName: cf.domainName 
         })
     }
 }
